6bd19f8a72f9592edf3876a02236217e1fc4e459
PIO-3 <163719626+PIO-3@users.noreply.github.com>
2024-05-18 10:05:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 let BrigCredits = 0;
 
 document.getElementById('start-button').addEventListener('click', function() {
-
     document.getElementById('content').classList.remove('hidden');
     this.style.display = 'none';
-    
 
     if (document.documentElement.requestFullscreen) {
         document.documentElement.requestFullscreen();
-    } else if (document.documentElement.mozRequestFullScreen) { /* Firefox */
+    } else if (document.documentElement.mozRequestFullScreen) {
         document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    } else if (document.documentElement.webkitRequestFullscreen) {
         document.documentElement.webkitRequestFullscreen();
-    } else if (document.documentElement.msRequestFullscreen) { /* IE/Edge */
+    } else if (document.documentElement.msRequestFullscreen) {
         document.documentElement.msRequestFullscreen();
     }
 });
@@ -20,7 +18,7 @@ document.getElementById('start-button').addEventListener('click', function() {
 document.getElementById('main-image').addEventListener('click', function() {
     BrigCredits += 1;
     document.getElementById('credits-value').textContent = BrigCredits;
-    
+
     const image = this;
     const randomTransform = [
         'rotate(20deg)',
@@ -32,13 +30,33 @@ document.getElementById('main-image').addEventListener('click', function() {
         'translateY(20px)',
         'translateY(-20px)'
     ];
-    
+
     const randomIndex = Math.floor(Math.random() * randomTransform.length);
     const originalTransform = image.style.transform;
-    
+
     image.style.transform = randomTransform[randomIndex];
-    
+
     setTimeout(() => {
         image.style.transform = originalTransform;
     }, 500);
+});
+
+document.getElementById('quit-button').addEventListener('click', function() {
+    const dialogBackground = document.createElement('div');
+    dialogBackground.id = 'dialog-background';
+    document.body.appendChild(dialogBackground);
+
+    document.getElementById('confirmation-dialog').classList.remove('hidden');
+});
+
+document.getElementById('yes-button').addEventListener('click', function() {
+    window.close();
+});
+
+document.getElementById('no-button').addEventListener('click', function() {
+    document.getElementById('confirmation-dialog').classList.add('hidden');
+    const dialogBackground = document.getElementById('dialog-background');
+    if (dialogBackground) {
+        dialogBackground.remove();
+    }
 });
